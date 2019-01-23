bd74b4342ba7d123bfbfb600ef4f346d2e5efe9b
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-23 12:30:16 -0700

    Updates Test card to have a default variable for Guess

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 class Turn
   attr_reader :question, :answer, :guess
-  def initialize(question, answer, guess)
+  def initialize(question, answer, guess = answer)
     @question = question
     @answer = answer
     @guess = guess
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card'
+require './lib/turn'
 require 'pry'
 
 class TurnTest < Minitest::Test

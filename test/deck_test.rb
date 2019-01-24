e891327ab2dd1fdfc6d8a0c5b0d901ce5221df67
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-24 16:56:03 -0700

    Adds Deck Test

---
+++ test/deck_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/card'
+require './lib/deck'
+require 'pry'
+
+class TurnTest < Minitest::Test
+
+  def test_it_exists
+    cards = [1, 2, 3]
+    deck = Deck.new(cards)
+    assert_instance_of Deck, deck
+  end
+
+  def test_cards_are_in_an_array
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+    assert card_1, deck.cards
+    assert card_2, deck.cards
+    assert card_3, deck.cards
+  end
+end

b24a1d63515e2dce23ac5205cfdd69dd1aab3e4e
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-23 17:59:08 -0700

    Completes turn_test and updates Turn Class

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,27 @@
 class Turn
   attr_reader :string, :card
-  def initialize(string, card)
+  def initialize(guess, card)
+    @string = guess
+    @card = card
   end
 
   def guess
+    return string
   end
 
   def correct?
+    if string == guess
+      return true
+    else
+      return false
+    end
   end
 
   def feedback
-  end 
+    if correct? == true
+      return "Correct!"
+    elsif correct? != true
+      return "Incorrect!"
+    end
+  end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -7,10 +7,27 @@ require 'pry'
 class TurnTest < Minitest::Test
 
   def test_it_exists
-    card = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, card
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_instance_of Turn, turn
   end
 
+  def test_it_has_a_guess
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Juneau", turn.guess
+  end
+
+  def test_is_it_correct
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal true, turn.correct?
+  end
+
+  def test_it_has_feedback
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Juneau", card)
+    assert_equal "Correct!", turn.feedback
+  end
 
 end

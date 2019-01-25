81eecc8609d294dc68d89353c89497366f06c35c
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-25 11:43:36 -0700

    Updates turn_test to change string to guess

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,6 +12,12 @@ class TurnTest < Minitest::Test
     assert_instance_of Turn, turn
   end
 
+def test_turn_has_a_card
+  card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  turn = Turn.new("Juneau", card)
+  assert_equal card, turn.card
+end
+
   def test_it_has_a_guess
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
     turn = Turn.new("Juneau", card)
@@ -24,6 +30,12 @@ class TurnTest < Minitest::Test
     assert_equal true, turn.correct?
   end
 
+  def test_is_card_incorrect
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    turn = Turn.new("Fargo", card)
+    assert_equal false, turn.correct?
+  end
+
   def test_it_has_feedback
 
     card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
@@ -31,8 +43,8 @@ class TurnTest < Minitest::Test
     assert_equal "Correct!", turn.feedback
   end
 
-  def test_is_it_incorrect
-    
+  def test_is_feedback_incorrect
+
     card = Card.new("Which planet is closest to the sun?", "Mercury", :STEM)
     turn = Turn.new("Saturn", card)
     assert_equal "Incorrect!", turn.feedback

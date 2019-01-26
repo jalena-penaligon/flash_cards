0efa335dee9d098f7dc4d060bc5b8b6d39fb43a0
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-01-26 15:10:04 -0700

    Adds current_card, take_turn, number_correct, and number_correct_by_category

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,32 @@
 class Round
-  attr_reader :deck
+  attr_reader :deck, :turns, :turn_count
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @turn_count = 0
   end
+
+  def current_card
+    @deck.cards[turn_count]
+  end
+
+  def take_turn(guess)
+    a_turn = Turn.new(guess, current_card)
+    @turns << a_turn
+    @turn_count += 1
+    return a_turn
+  end
+
+  def number_correct
+    @turns.select do |turn|
+      turn.correct?
+    end
+  end
+
+  def number_correct_by_category(category)
+    @turns.select do |turn|
+      turn.card.category == category && turn.correct?
+    end
+  end
+
 end
